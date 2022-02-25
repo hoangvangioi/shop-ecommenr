5ee4c0b8eb5643dbc310105c2aea927adb15fe14
gioitube <gioitube2k2@gmail.com>
2022-02-25 23:08:19 +0700

    demo v_0.1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ch8mk1u#$ns-51sw24oo2n7%on*woka$n)62p_k^mlgrp33x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ecommenr-django.herokuapp.com","*","127.0.0.1"]
 
 
 # Application definition
